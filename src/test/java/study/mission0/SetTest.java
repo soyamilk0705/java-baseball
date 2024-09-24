755ecc032e1048a562bf36641b08c27e872b4b71
Heesoo <soyamilk0705@naver.com>
2024-09-24 23:19:29 +0900

    [docs] 필요없는 주석 삭제

--- src/test/java/study/mission0/SetTest.java
+++ src/test/java/study/mission0/SetTest.java
@@ -25,22 +25,14 @@ public class SetTest {
         numbers.add(3);
     }
 
-    /**
-     * [요구사항 1]
-     * Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.
-     */
+
     @Test
     @DisplayName("요구사항 1: Set 의 크기를 확인한다.")
     void checkSize_success(){
         assertThat(numbers.size()).isEqualTo(4);
     }
 
-    /**
-     * [요구사항 2]
-     * Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
-     * 구현하고 보니 다음과 같이 중복 코드가 계속해서 발생한다.
-     * JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.
-     */
+
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
     @DisplayName("요구사항 2: contains() 메소드를 활용해 값 존재하는지 확인하며 ParameterizedTest 를 이용해 중복코드를 제거한다.")
@@ -49,15 +41,10 @@ public class SetTest {
     }
 
 
-    /**
-     * [요구사항 3]
-     * 요구사항 2는 contains 메소드 결과 값이 true인 경우만 테스트 가능하다. 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현한다.
-     * 예를 들어 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현한다.
-     */
     @ParameterizedTest
     @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
     @DisplayName("요구사항 3: contains() 메소드를 활용해 값 존재하는지 확인하며 입력값에 따라 true, false 를 반환한다.")
-    void containsTest_expectedTrueOrFalse_success(int input, boolean expected){
+    void isContains_returnTrueOrFalse_success(int input, boolean expected){
         assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 
--- src/test/java/study/mission0/StringTest.java
+++ src/test/java/study/mission0/StringTest.java
@@ -13,11 +13,7 @@ public class StringTest {
         assertThat(actual).isEqualTo("adc");
     }
 
-    /**
-     * 요구사항 1
-     * "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
-     * "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
-     */
+
     @Test
     @DisplayName("요구사항 1 : split() 함수를 이용해 문자열을 분리시킨다.")
     void split_success(){
@@ -26,10 +22,7 @@ public class StringTest {
         assertThat(splitValues).containsExactly("1", "2"); // 원소가 정확히 일치하는지 확인
     }
 
-    /**
-     * 요구사항 2
-     * "(1,2)" 값이 주어졌을 때 String 의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.
-     */
+
     @Test
     @DisplayName("요구사항 2 : substring() 함수를 이용해 특정 문자열을 제거 시킨다.")
     void substring_success(){
@@ -38,15 +31,10 @@ public class StringTest {
         assertThat(subValues).isEqualTo("1,2");
     }
 
-    /**
-     * 요구사항 3
-     * "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
-     * String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
-     * JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
-     */
+
     @Test
     @DisplayName("요구사항 3 : chatAt() 메소드를 이용해 StringIndexOutOfBoundsException 을 발생시킨다.")
-    void chatAt_StringIndexOutOfBoundsException_fail(){
+    void chatAt_StringIndexOutOfBoundsException(){
         String value = "abc";
         assertThrows(StringIndexOutOfBoundsException.class, () -> value.charAt(value.length()+1));
     }
